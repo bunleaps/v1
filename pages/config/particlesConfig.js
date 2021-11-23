6d7a550c6aa5ac073fa20c5d2829d6a810f89c00
bunleaps <bunleap.sorn@gmail.com>
2021-11-23 14:36:36 +0700

    component: add react-particles-js@2.4.0 bg

---
+++ pages/config/particlesConfig.js
@@ -0,0 +1,26 @@
+const particlesConfig = {
+    /*   
+      "particles": {
+        ...
+      }, 
+      "retina_detect": true
+    */
+    "particles": {
+        "number": {
+            "value": 60
+        },
+        "size": {
+            "value": 4
+        }
+    },
+    "interactivity": {
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            }
+        }
+    }
+  };
+  
+export default particlesConfig;
